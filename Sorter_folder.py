e81dbcd1a41884ec6c496ac917ff8b15a12498a8
BonesetterB <122158929+BonesetterB@users.noreply.github.com>
2023-05-28 16:35:32 +0300

    х

---
+++ Sorter_folder.py
@@ -0,0 +1,104 @@
+import os
+import shutil
+from zipfile import ZipFile
+from threading import Thread, RLock
+from time import time
+import logging
+
+lock=RLock()
+
+path = 'D:/Мотлох/'
+list_ = os.listdir(path)
+slovn = {'video': ['AVI', 'MP4', 'MOV', 'MKV'],
+         'images': ['JPEG', 'PNG', 'JPG', 'SVG', 'BMP'],
+         'documents': ['DOC', 'DOCX', 'TXT', 'PDF', 'XLSX', 'PPTX'],
+         'audio': ['MP3', 'OGG', 'WAV', 'AMR'],
+         'archives': ['ZIP', 'GZ', 'TAR'], }
+
+
+
+def move(type,re_name_path,re_name):
+    if os.path.exists(path+'/'+type):
+        shutil.move(re_name_path, path+'/'+type+'/'+re_name)
+    else:
+        os.makedirs(path+'/'+type)
+        shutil.move(re_name_path, path+'/'+type+'/'+re_name)
+
+
+
+def zip_unpack(re_name_path,name):
+    if os.path.exists(path+'/'+'archives'):
+
+        path_zip = os.path.join(path+'/'+'archives', name)
+        os.mkdir(path_zip)
+        with ZipFile(re_name_path, 'r') as zip_file:
+            zip_file.extractall(
+                path=path+'/'+'archives'+'/'+name)
+            os.remove(re_name_path)
+    else:
+        os.makedirs(path+'/'+'archives')
+        with ZipFile(re_name_path, 'r') as zip_file:
+            zip_file.extractall(
+                path=path+'/'+'archives'+'/'+name)
+
+        os.remove(re_name_path)
+
+
+def normalize(name):
+    Ukr = "абвгдежзийклмнопрстуфхцчшщъыьэюяєіїґ"
+    Eng = ("a", "b", "v", "g", "d", "e", "e", "j", "z", "i", "j", "k", "l", "m", "n", "o", "p", "r", "s", "t", "u",
+           "f", "h", "ts", "ch", "sh", "sch", "", "y", "", "e", "yu", "ya", "je", "i", "ji", "g")
+    TRANS = {}
+    for c, l in zip(Ukr, Eng):
+        TRANS[ord(c)] = l
+        TRANS[ord(c.upper())] = l.upper()
+    return name.translate(TRANS)
+
+
+
+
+def sort(locker,list_):
+        for file_ in list_:
+            locker.acquire()
+            logging.debug(file_)
+            name, ext = os.path.splitext(file_)
+            print(name)
+            file_ = os.path.join(path, file_)
+            ext = ext[1:]
+            if ext == '':
+                if name != 'images' or name != 'documents' or name != 'audio' or name != 'video' or name != 'archives':
+                    if not os.listdir(file_):
+                            os.rmdir(file_)
+                    else:
+                        continue
+            else:
+                    re_name = normalize(name)+'.'+ext
+                    re_name_path = path+re_name
+                    os.rename(file_, re_name_path)
+                    for k, v in slovn.items():
+                        if ext.upper() in v:
+                            try:
+                                if k != 'archives':
+                                    move(k,re_name_path,re_name)
+                                else:
+                                    zip_unpack(re_name_path,name)
+                            except FileNotFoundError:
+                                continue
+                        else:
+                            continue
+            locker.release()
+
+
+
+timer = time()
+logging.basicConfig(level=logging.DEBUG, format='%(threadName)s %(message)s')
+t1=Thread(target=sort, args=(lock,list_))
+t2=Thread(target=sort, args=(lock,list_))
+threads=[t1,t2]
+t1.start()
+t2.start()
+
+[el.join() for el in threads]
+
+print('yes')
+logging.debug(f'{time()-timer}')
